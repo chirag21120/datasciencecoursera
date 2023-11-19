a9f27be9535f6cc868f9d7860891f6d6dbf84f93
CHIRAG <cgupta_be21@thapar.edu>
2023-11-19 14:24:40 +0530

    changed

--- ProgramingAssigment2.R
+++ ProgramingAssigment2.R
@@ -3,24 +3,24 @@
 
 ## Write a short comment describing this function
 # This function is responsible for set, get, setinverse and getinverse operations
-makeCacheMatrix <- function(x = matrix()) {
+createCacheMatrix <- function(x = matrix()) {
   # Initialize the inverse_matrix 
-  inverse_matrix <- NULL
+  inverse.matrix <- NULL
   
   # Function to set the matrix
   set <- function(mat){
     x <<- mat
-    inverse_matrix <<- NULL
+    inverse.matrix <<- NULL
   }
   
   # Function to get the matrix 
   get <- function() x  
   
   # Function to set the inverse matrix
-  setinverse <- function(im) inverse_matrix <<- im 
+  setinverse <- function(im) inverse.matrix <<- im 
   
   # Function to get the inverse matrix
-  getinverse <- function() inverse_matrix
+  getinverse <- function() inverse.matrix
   
   list(set = set, get = get,
        setinverse = setinverse,
@@ -34,27 +34,27 @@ cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
   
   # Retrieve the inverse matrix
-  inverse_matrix <- x$getinverse()
+  inverse.matrix <- x$getinverse()
   
   # If the inverse matrix is cached, return it
-  if(!is.null(inverse_matrix)) {
+  if(!is.null(inverse.matrix)) {
     message("getting cached data")
-    return(inverse_matrix)
+    return(inverse.matrix)
   }
   
   # If the inverse matrix is not cached, calculate it
   data <- x$get()
-  inverse_matrix <- solve(data, ...)
+  inverse.matrix <- solve(data, ...)
   
   # Cache the calculated inverse matrix
-  x$setinverse(inverse_matrix)
+  x$setinverse(inverse.matrix)
   
   # Return the calculated inverse matrix
-  inverse_matrix
+  inverse.matrix
 }
 
 ## Sample Example for Testing
-myMatrix <- makeCacheMatrix(matrix(c(1,2,1,3,4,5,2,1,3), nrow = 3, byrow = TRUE))
+myMatrix <- createCacheMatrix(matrix(c(1,2,1,3,4,5,2,1,3), nrow = 3, byrow = TRUE))
 
 cat("Original Matrix:\n")
 print(myMatrix$get())
